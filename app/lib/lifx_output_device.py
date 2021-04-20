2492d6853de315f0960d402e4c36fb57e627ceab
Elie Rotenberg <elie@rotenberg.io>
2021-04-20 19:00:17 +0200

    Improve Lifx output resilience

--- app/lib/lifx_output_device.py
+++ app/lib/lifx_output_device.py
@@ -1,11 +1,15 @@
 from app.lib.color import RGBColor
 from app.lib.output_device import OutputDevice
+import lifxlan  # type: ignore
 from lifxlan import Light  # type: ignore
 
 INFINITE_CYCLES = 1000000
 
 LOW_BRIGHTNESS = 0
 HIGH_BRIGHTNESS = int(65535 / 2)
+
+lifxlan.DEFAULT_ATTEMPTS = 5
+lifxlan.DEFAULT_TIMEOUT = 5
 
 
 class LifxOutputDevice(OutputDevice):
@@ -16,17 +20,18 @@ class LifxOutputDevice(OutputDevice):
         self._light = Light(mac_addr=mac_address, ip_addr=ip_address)
 
     def on(self, color: RGBColor):
-        self._light.set_power("off")  # type: ignore
-        self._light.set_power("on")  # type: ignore
-        self._light.set_color(color=color.to_lifx_colors(LOW_BRIGHTNESS))  # type: ignore
+        self._light.set_power("off", rapid=True)  # type: ignore
+        self._light.set_power("on", rapid=True)  # type: ignore
+        self._light.set_color(color=color.to_lifx_colors(HIGH_BRIGHTNESS), rapid=True)  # type: ignore
         self._light.set_waveform(  # type: ignore
             is_transient=1,
-            color=color.to_lifx_colors(HIGH_BRIGHTNESS),
+            color=color.to_lifx_colors(LOW_BRIGHTNESS),
             period=2000,
             cycles=INFINITE_CYCLES,
             duty_cycle=0.5,
             waveform=1,
+            rapid=True,
         )
 
     def off(self):
-        self._light.set_power("off")  # type: ignore
+        self._light.set_power("off", rapid=True)  # type: ignore
