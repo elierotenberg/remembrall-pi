5910db51ab46813516d4f61688b9b84608691c26
Elie Rotenberg <elie@rotenberg.io>
2021-04-21 15:46:19 +0200

    Remove "Rapid" flag from Lifx output

--- app/lib/lifx_output_device.py
+++ app/lib/lifx_output_device.py
@@ -20,9 +20,9 @@ class LifxOutputDevice(OutputDevice):
         self._light = Light(mac_addr=mac_address, ip_addr=ip_address)
 
     def on(self, color: RGBColor):
-        self._light.set_power("off", rapid=True)  # type: ignore
-        self._light.set_power("on", rapid=True)  # type: ignore
-        self._light.set_color(color=color.to_lifx_colors(HIGH_BRIGHTNESS), rapid=True)  # type: ignore
+        self._light.set_power("off")  # type: ignore
+        self._light.set_power("on")  # type: ignore
+        self._light.set_color(color=color.to_lifx_colors(HIGH_BRIGHTNESS))  # type: ignore
         self._light.set_waveform(  # type: ignore
             is_transient=1,
             color=color.to_lifx_colors(LOW_BRIGHTNESS),
@@ -34,4 +34,4 @@ class LifxOutputDevice(OutputDevice):
         )
 
     def off(self):
-        self._light.set_power("off", rapid=True)  # type: ignore
+        self._light.set_power("off")  # type: ignore
